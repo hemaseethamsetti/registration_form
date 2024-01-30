e930fc8effaf858080348894453baf3eeb2691ce
hema <hema.seethamsetti@sasi.ac.in>
2024-01-30 21:22:44 +0530

    regform

---
+++ App.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+    
+  };
+
+  return (
+    <div className="App">
+      <h1>Registration Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Phone number:
+          <input
+            type="number"
+            name="number"
+            value={formData.number}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <center><button type="submit">Register</button></center>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+
